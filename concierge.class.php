b29666aa3cee6de6e0650b26ba33649dedea9bdc
FlavienCERIANI <flaviencer16@gmail.com>
2016-12-14 16:21:17 +0100

    class fille concierge

---
+++ concierge.class.php
@@ -0,0 +1,17 @@
+<?php
+require ("user.class.php");
+class Concierge extends user{
+  protected $_affect
+  protected $_data
+  protected $_comptepre
+
+
+  parent::__construct($Nom, $Prenom, $IDbadge);
+  $this->
+}
+
+
+public function lire(){
+  $id = fopen($this->)
+}
+ ?>

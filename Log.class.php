b271e5697ef1d29b8543b28d76f9f67e351b7f91
Patricia Geraci <limmagin@hotmail.fr>
2016-12-14 15:26:24 +0100

    class stat et log

---
+++ Log.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class log(){
+
+  private $id_Equip  //int not null
+          $id_user   //int not null
+          $id_debut  //date not null
+          $id_fin    //date not null
+          $id_lieu   //int not null
+          $id_animation   //int null
+
+  //methode qui recevra les requetes des entiers
+
+  public log-request (id_Equip,id_user){
+    return true;//boolean
+    }
+  }
+
+ ?>
---
+++ Stats.class.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class stat{ 
+    public $stat;
+  }
+public get-data ($datedebut,$datefin,$id_Equi = null,.. ){
+  return $data;//JsonSerializable
+
+public get-data-graph ($datedebut, $datefin){
+        $data=this->get-data();
+    return $img;//jpeg2wbmp
+  }
+
+public get-data-table ($dd,$df){
+  //données factices
+  //table: <table>
+  //       <id><tr>
+  //       </table>
+  }
+ }
+}
+
+
+?>

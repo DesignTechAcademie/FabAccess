5623a8a920d28c1567713269367373761a1ace9c
David Réchatin <david.rechatin@openscop.fr>
2016-12-19 12:04:24 +0100

    Reception d'une requete envoyée par un Raspberry

--- request.php
+++ request.php
@@ -11,7 +11,24 @@
 
   $tag = $_GET['tag'];
   $addr = $_GET['addr'];
-  
+
+  // données factices
+  $id_equipement = "10";
+  $id_user = "5";
+  $id_lieu = "32971823";
+
+
+  /*
   $badge = new Badge($tag)
   $uid_user = $badge->get_uid();
+  */
+
+
+  $monlog = new Log();
+  $result = $monlog->record($id_equipement, $id_user, $id_lieu);
+  echo $result;
+
+
+
+
 ?>

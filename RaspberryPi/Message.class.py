7f35ad58e9c7a7cf5239758f643b33eb6f54ad44
Christophe-Junier <christophejunier@gmail.com>
2016-12-19 10:27:29 +0100

    Correction de quelques erreur

--- RaspberryPi/Message.class.py
+++ RaspberryPi/Message.class.py
@@ -1,29 +1,32 @@
 class Message:
-#toutes les ligne "print" sont vouées à disparaitre elle sont uniquement là pour les batteries de tests.
-    def __init__(self, username):
+#toutes les ligne "print" sont vouées à disparaitre elle sont uniquement là pour les batteries de tests. (christophe)
+
+    def __init__(self, username):      #initialisation de la class avec le nom de la personne visée en paramètre.
         self._username = username
 
-    def afficherBonjour(self):
+    def afficherBonjour(self):         #on dit bonjour à la personne.
         Message = "Bonjour " + self._username + " !"
         print(Message)
 
-    def afficherAurevoir(self):
+    def afficherAurevoir(self):        #on lui dit aussi aurevoir un peu de politesse.
         Message = "Aurevoir " + self._username + " !"
         print(Message)
 
-    def afficherConcierge(self):
-        Message = "Bonjour " + self._username + ".Veuillez vous rendre auprès d'un gérant."
+    def afficherMessage(self,message):    #on lui dit qu'il/elle a des messages, puis on les affiches.
+        Message = "Bonjour " + self._username + ".Vous avez un/des nouveau(x) message."
         print(Message)
+        print("\n")
+        print(message)
 
-    def afficherPrix(self,prix):
+    def afficherPrix(self,prix):        #affiche le prix d'une unité sur la machine concerné.
         Message = "1 unité sur cette machine coute " + prix
         print(Message)
 
-    def declarerIncident(self):
+    def declarerIncident(self):         #Demande à l'utilisateur si il souhaite declarer un incident.
         Message = self._username + " vous souhaitez déclarer un incident ?"
         print(Message)
 
-    def afficherCredit(self, credit):
+    def afficherCredit(self, credit):      #affiche les credits de l'utilisateurs, ou qu'il n'en a plus.
         if(credit == "0):
             Message = "Vous n'avez plus de crédits"
             print(Message)

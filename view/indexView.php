e84d8e948df60488343213732ad881902eb1c7fc
David Réchatin <david.rechatin@openscop.fr>
2016-12-19 12:19:20 +0100

    Afficher les évènements du jours

--- view/indexView.php
+++ view/indexView.php
@@ -1,3 +1,10 @@
+<?php
+spl_autoload_register(function ($class) {
+    include '../Class/' . $class . '.class.php';
+});
+?>
+
+
 <main>
 	<div id ="row">
 		<h2>Interface Admin</h2>
@@ -5,6 +12,20 @@
 	</div>
     <div class="row">
         <!-- <p>alalala</p> -->
+
+        <h2>Evenements du jour</h2>
+        <?php
+        $agenda = new Agenda();
+        $event_du_jour = $agenda->get("32971823");
+
+        if (is_array($event_du_jour)) {
+            echo $event_du_jour[1] . " - Lieu : " . $event_du_jour[2];
+        } else {
+            echo $event_du_jour;
+        }
+
+        ?>
+
     </div>
   </div>
 </main>

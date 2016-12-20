55b688ea2dc8f1f1054f943958635187c3480d7b
Christophe-Junier <christophejunier@gmail.com>
2016-12-20 09:25:43 +0100

    ajout commentaire

--- RaspberryPi/badge.py
+++ RaspberryPi/badge.py
@@ -4,10 +4,10 @@
 import serial
 import requests
 
-serial = serial.Serial("emplacement du port usb", baudrate=9600)
-tag = ''
+serial = serial.Serial("emplacement du port usb", baudrate=9600) #on initialise serial sur l'emplacement de la carte RFID
+tag = ''   #le tag est initialiser à vide
 
-while True:
+while True:                            #tant qu'une carte est detecter on recupere le tag rfid
 		data = serial.read()
 		if data == '\r'
 				tag = ''
@@ -16,8 +16,8 @@ while True:
     
 
           
-requests.post("adresse du serveur", data = tag) # Envoie la réponse au serveur.
+requests.post("adresse du serveur", data = tag) # Envoie la réponse au serveur.   #quand la boucle est faite on envoie le tag au serveur
           
 
         
-time.sleep(2000)
+time.sleep(2000) #2 seconde entre deux badgage.

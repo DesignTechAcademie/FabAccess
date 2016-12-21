fee9d4b18030bbc29c862ed9d570f318301ae805
TamimeBelguendouz <tamime.belguendouz@gmail.com>
2016-12-21 11:08:10 +0100

    Modification et ajout méthode

--- Class/FabManager.class.php
+++ Class/FabManager.class.php
@@ -6,6 +6,7 @@
 
 //On appelle la classe FichierCSV une fois
 require_once('FichierCSV.class.php');
+require ('Article.class.php');
 
 /**
  * Class FabManager
@@ -24,7 +25,7 @@ class FabManager extends Concierge{
      */
     public function __construct($Nom, $Prenom, $ID, $dateInscrip){
 
-      parent::__construct($Nom, $Prenom, $ID, $dateInscrip);
+        parent::__construct($Nom, $Prenom, $ID, $dateInscrip);
 
     }
 
@@ -36,9 +37,9 @@ class FabManager extends Concierge{
      * @param $droitEquip
      */
     public function ecrire_droit_equip($ID, $nomEquip, $droitEquip){
-      $monfichier = new FichierCSV($nomEquip."_droits");//On crée un nouvel objet qui est un fichier CSV en indiquant le nom de l'équipement
-      $list = [$ID, $droitEquip];//On crée deux colonnes contenant l'identifiant du membre et son droit d'accès
-      $monfichier-> write_csv($list);//On écrit dans les colonnes
+        $monfichier = new FichierCSV($nomEquip."_droits");//On crée un nouvel objet qui est un fichier CSV en indiquant le nom de l'équipement
+        $list = [$ID, $droitEquip];//On crée deux colonnes contenant l'identifiant du membre et son droit d'accès
+        $monfichier-> write_csv($list);//On écrit dans les colonnes
 
     }
 
@@ -49,24 +50,24 @@ class FabManager extends Concierge{
      * @param $nomEquip
      */
     public function ecrire_affectation_boitier($IDBoitier, $nomEquip){
-      $monfichier = new FichierCSV("Affectation_Boitier");//On crée un nouvel objet qui est un fichier CSV unique
-      $list = [$IDBoitier, $nomEquip];//On crée deux colonnes contenant l'identifiant du boitier et le nom de l'équipement
-      $monfichier-> write_csv($list);//On écrit dans les colonnes
+        $monfichier = new FichierCSV("Affectation_Boitier");//On crée un nouvel objet qui est un fichier CSV unique
+        $list = [$IDBoitier, $nomEquip];//On crée deux colonnes contenant l'identifiant du boitier et le nom de l'équipement
+        $monfichier-> write_csv($list);//On écrit dans les colonnes
 
     }
 
     /**
-     * Méthode pour créer un fichier CSV qui contient le prix des consommables
+     * On appelle la méthode de la classe Article pour modifier le prix des consommables
      *
-     * @param $nomEquip
+     * @param $idConso
      * @param $conso
      * @param $prix
+     * @return string
      */
-    public function ecrire_tarifs_equip($nomEquip, $conso, $prix){
-      $monfichier = new FichierCSV($nomEquip."_conso");//On crée un fichier CSV pour chaque équipement
-      $list = [$conso, $prix];//On crée deux colonnes contenant le nom du consommable et le prix
-      $monfichier-> write_csv($list);//On écrit dans les colonnes
-
+    public function ajouter_article($idConso, $conso, $prix){
+        $article = new Article;
+        $result = $article->ajouter($idConso, $conso, $prix);
+        return $result;
     }
 
     /**
@@ -77,6 +78,10 @@ class FabManager extends Concierge{
     }
 
 }
+
+$articles = new FabManager();
+$result = $articles->ajouter_article(4,'plastique', 3);
+var_dump($result);
 
 
 ?>

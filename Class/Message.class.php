b693d2e52e72c674d21cba883a72a20b204432ae
Christophe-Junier <christophejunier@gmail.com>
2016-12-21 09:40:14 +0100

    ajout de la class message

---
+++ Class/Message.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Christophe / Yacine
+ * Date: 21/12/16
+ * Time: 08:45
+ */
+class Message
+{
+
+private $_IDmessages; // le numéro du message
+private $_messages; // le message
+private $_IDuser; // le destinataire
+private $_dateConfirm; // la date de confirmation de lecture du message
+
+
+
+    public function enregisterMessages($text,$user){
+
+
+
+    }
+
+    public function getMessages($user){
+
+    }
+
+    public function  getAllMessage(){
+
+    }
+
+
+    public function confirmeMessages($IDMessage){
+
+
+    }
+}
